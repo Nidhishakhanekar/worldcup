226c54ff3dc6e0ea239295fd36950029aeab52e9
nidhishakhanekar <nidhishakhanekar@gmail.com>
2025-01-11 22:37:29 +0530

    dragNdrop

---
+++ testcase/test_dragandDrop.py
@@ -0,0 +1,16 @@
+import time
+
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+
+class Test_Drag:
+
+    def test_dragndrop(self,Setup):
+        self.driver=Setup
+        self.driver.find_element(By.XPATH,"//a[text()='Drag and Drop']").click()
+        A=self.driver.find_element(By.XPATH,"//div[contains(@id,'column-a')]")
+        B=self.driver.find_element(By.XPATH, "//div[contains(@id,'column-a')]")
+        Ac=ActionChains(self.driver)
+        Ac.drag_and_drop(A,B)
+        time.sleep(5)
