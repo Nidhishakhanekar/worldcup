7a3f887403693ca3739d83f6b68052cce0b48aeb
nidhishakhanekar <nidhishakhanekar@gmail.com>
2025-01-11 23:05:16 +0530

    changes

--- testcase/test_dragandDrop.py
+++ testcase/test_dragandDrop.py
@@ -6,11 +6,11 @@ from selenium.webdriver.common.by import By
 
 class Test_Drag:
 
-    def test_dragndrop(self,Setup):
-        self.driver=Setup
-        self.driver.find_element(By.XPATH,"//a[text()='Drag and Drop']").click()
-        A=self.driver.find_element(By.XPATH,"//div[contains(@id,'column-a')]")
-        B=self.driver.find_element(By.XPATH, "//div[contains(@id,'column-a')]")
-        Ac=ActionChains(self.driver)
-        Ac.drag_and_drop(A,B)
+    def test_dragndrop(self, Setup):
+        self.driver = Setup
+        self.driver.find_element(By.XPATH, "//a[text()='Drag and Drop']").click()
+        A = self.driver.find_element(By.XPATH, "//div[contains(@id,'column-a')]")
+        B = self.driver.find_element(By.XPATH, "//div[contains(@id,'column-b')]")
+        Ac = ActionChains(self.driver)
+        Ac.drag_and_drop(A, B).perform()
         time.sleep(5)
